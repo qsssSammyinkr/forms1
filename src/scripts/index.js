3c62e16447b721f3aa3ad94fdbbf09cd00f42fe1
qsssSammyinkr <hafbdjjt@gmail.com>
2025-10-31 04:41:08 +0000

    ⚙️ Auto commit - 2025-10-31 04:41:08

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,12 +2,12 @@ const container = document.querySelector('.container');
 const SignUpLink = document.querySelector('.SignUpLink');
 const SignInLink = document.querySelector('.SignInLink');
 
-SignUpLink.addEventListener('click', e => {
+SignUpLink.addEventListener('click', (e) => {
   e.preventDefault();
   container.classList.add('active');
 });
 
-SignInLink.addEventListener('click', e => {
+SignInLink.addEventListener('click', (e) => {
   e.preventDefault();
   container.classList.remove('active');
 });
