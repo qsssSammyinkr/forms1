f6004d92dfd5249acff78225a7eb52c22cd1e42c
qsssSammyinkr <hafbdjjt@gmail.com>
2025-10-31 03:24:14 +0000

    ⚙️ Auto commit - 2025-10-31 03:24:14

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
 const container = document.querySelector('.container');
 const LoginLink = document.querySelector('.SignInLink');
-const RegisterLink = document.querySelector('.SignUpLinl')
+const RegisterLink = document.querySelector('.SignUpLinl');
+RegisterLink.addEventListener('click', () => {
+    container.classList.add
+})
